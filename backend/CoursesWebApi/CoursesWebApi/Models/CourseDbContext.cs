a9ea92c3a8c494cd861b3940af1bf4203ae2fe64
Syed Asjad Alam <asjadakmal1@gmail.com>
2022-09-06 18:31:21 +0500

    for reviewing

---
+++ backend/CoursesWebApi/CoursesWebApi/Models/CourseDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CoursesWebApi.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoursesWebApi.Models
+{
+    public class CourseDbContext:DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentCourses> StudentCourses { get; set; }
+        public DbSet<AllocatedCourses> AllocatedCourses { get; set; }
+
+
+
+
+
+        public CourseDbContext(DbContextOptions<CourseDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
